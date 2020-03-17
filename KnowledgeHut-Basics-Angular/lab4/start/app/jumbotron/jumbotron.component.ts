31185379ee9287d3723278c97b0d5773c9257212
alcfeoh <alain@interstate21.com>
2020-03-17 15:21:39 -0700

    Lab 4 solution

---
+++ KnowledgeHut-Basics-Angular/lab4/start/app/jumbotron/jumbotron.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-jumbotron',
+  templateUrl: './jumbotron.component.html',
+  styleUrls: ['./jumbotron.component.css']
+})
+export class JumbotronComponent  {
+
+  @Input()
+  title;
+
+  @Input()
+  description;
+
+
+  constructor() { }
+}
