9f29f912b9b183c8b43a5781072c4437f637d12a
alcfeoh <alain@interstate21.com>
2020-03-17 15:52:01 -0700

    Lab 6 KH solution

---
+++ KnowledgeHut-Basics-Angular/lab6/start/app/hello/hello.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello',
+  template: `
+    <div>
+      <h2>Hello {{firstName}} {{name}}</h2>
+    </div>
+  `
+})
+export class HelloComponent {
+
+  name: string;
+
+  firstName = 'John';
+
+  constructor() {
+    this.name = 'Angular';
+  }
+
+}
