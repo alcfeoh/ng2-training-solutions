95b3a993be485a07fcfb2aa7a793f345e0d1e3ea
alcfeoh <alain@interstate21.com>
2020-03-17 15:32:27 -0700

    Lab 5 KH solution

---
+++ KnowledgeHut-Basics-Angular/lab5/end/app/license-plate/license-plate.component.spec.ts
@@ -0,0 +1,46 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LicensePlateComponent } from './license-plate.component';
+import {CALIFORNIA_PLATE, LICENSE_PLATES} from "../mock-data";
+import {By} from "@angular/platform-browser";
+
+describe('LicensePlateComponent', () => {
+  let component: LicensePlateComponent;
+  let fixture: ComponentFixture<LicensePlateComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LicensePlateComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LicensePlateComponent);
+    component = fixture.componentInstance;
+    component.plate = CALIFORNIA_PLATE;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should NOT display a "sale" tag if the plate is not on sale', () => {
+    component.plate.onSale = false;
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('h2 img'))).toBeNull();
+  });
+
+  it('should display a "sale" tag if the plate is on sale', () => {
+    component.plate.onSale = true;
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('h2 img')).nativeElement.src).toContain('/assets/sale.png');
+  });
+
+  it('should display a Javascript alert when clicking on "Add to cart" ', () => {
+    spyOn(window, 'alert');
+    fixture.debugElement.query(By.css('button')).triggerEventHandler('click', {});
+    expect(window.alert).toHaveBeenCalledWith('Plate added to cart');
+  });
+})
